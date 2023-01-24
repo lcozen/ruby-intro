0940763c654e60a9e140dec46e9e179afd07bb12
lcozen <lindsay.cozen@kellogg.northwestern.edu>
2023-01-24 21:11:49 +0000

    save1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,12 +5,18 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 5
+puts 2
 
 # Perform simple math with numbers
+puts 5 * (2+1)
 
 # Strings
+puts "Hello, world!"
 
 # Combine strings together
+
+puts "tacos are" + " delish"
 
 # Variables
 
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -4,11 +4,27 @@
 
 # Booleans
 
+is_true = true 
+is_false = false
+puts is_true
+puts is_false
+
 # Boolean Expressions
 
+puts 3 > 2
+puts 3 < 2
+puts 3 == 2
+# x = 2
+puts 3 != 2
+
 # If Conditional Logic
+if true
+    # ....
+end
 
 # If/Else Conditional Logic
+
+if 3 > 2
 
 # Elsif Conditional Logic
 
